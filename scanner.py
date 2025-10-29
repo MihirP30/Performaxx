88a40c5038528ac879cf9de229d608a82a0e0f87
Loago Zambe <zambeloago@gmail.com>
2025-10-29 15:09:42 -0400

    made buttons work

--- scanner.py
+++ scanner.py
@@ -8,12 +8,11 @@ from io import BytesIO
 API_KEY = os.getenv("GEMINI_API_KEY") 
 client = genai.Client(api_key=API_KEY)
 
-image_response = None
-
-# *** IMPORTANT: Change the path to your actual image file ***
 IMAGE_PATH = r"C:\Users\zambe\Downloads\20251029_102651.jpg"
 PROMPT = "Analyse the perfomative maleness of this image, from 1 to 100. Assess it ONLY on presence of items from this list: matcha, labubu, feminine literature, baggy jeans, rings, tote bag, wired headphones, vintage clothing. Higher scores indicate a stronger presence of these items. Provide a score and a brief explanation. Return a list of all items that could be added from the list and REAL links to purchase them."
 GENERATION_PROMPT = "Generate a more performative male version of this image. Make sure all these items are somehow included in the image unless they already are: matcha, labubu, feminine literature, baggy jeans, rings, tote bag, wired headphones, vintage clothing. Keep background the same"
+AI_IMAGE_OUTPUT_PATH = r"more_ performative_image.jpg"
+
 
 def analyze_image(image_path: str) -> str:
     try:
@@ -24,9 +23,8 @@ def analyze_image(image_path: str) -> str:
             contents=[input_image, PROMPT]
         )
         
-        image_response = analysis_response.text
         print("\n Analysis Complete:")
-        print(image_response)
+        return input_image,analysis_response.text
 
 
     except FileNotFoundError:
@@ -53,12 +51,13 @@ def generate_more_performative_image(image_path: str):
                 image_data = part.inline_data.data
                 generated_image = Image.open(BytesIO(image_data))
                 
-                generated_image.save("more_performative_image.png")
-                generated_image.show()
-                break
-                
+                generated_image.save(AI_IMAGE_OUTPUT_PATH)
+                return True
+                break 
+        
     except Exception as e:
         print(f"An error occurred during image generation: {e}")
+        return False
 
 if __name__ == "__main__":
     
